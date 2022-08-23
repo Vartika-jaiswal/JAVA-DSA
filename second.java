7a15bcd66aab2379df00e80f8579e6272409b690
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-23 07:06:39 -1100

    Day-2 relational operator

--- second.java
+++ second.java
@@ -1,7 +1,7 @@
 public class second {
     public static void main(String[] arg){
-        int i = 230;
-        byte b = (byte)i;
-        System.out.println(b);
+        int a = 10;
+        int b = 20;
+        System.out.println(a<b);
     }
 }
