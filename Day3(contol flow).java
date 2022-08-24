34c733bdc90b46cc16c7b7899278c342ebf6ba72
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-24 06:59:02 -1100

    Day-3

--- Day3(contol flow).java
+++ Day3(contol flow).java
@@ -1,11 +1,25 @@
- class Day3 {
+import javax.lang.model.util.ElementScanner14;
+
+class Day3 {
     public static void main(String[] args){
-        int x = 20;
-        int y = 18;
-        if(y>x){
-            System.out.println("y is greater than x");
-        }else{
-            System.out.println("x is greater than y");
+        int Marks = 65;
+        if(Marks<50){
+            System.out.println("Fail");
+        }
+        else if(Marks>=50 && Marks<60){
+            System.out.println("D grade");
+        }
+        else if(Marks>=60 && Marks<75){
+            System.out.println("C grade");
+        }
+        else if(Marks>=75 && Marks<90){
+            System.out.println("B grade");
+        }
+        else if(Marks>=90 && Marks<100){
+            System.out.println("A+ grade");
+        }
+        else{
+            System.out.println("Invalid!");
         }
     }
     
