7389ae0db4648cc4021a3bfa2ba64d4a579e8453
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-24 08:26:02 -1100

    Day-3

--- Day_3(Loops).java
+++ Day_3(Loops).java
@@ -2,7 +2,7 @@ class Day_3{
     public static void main(String[] args){
         for(int i=0;i<5;i++){
             for(int j=0;j<=i;j+=1){
-                 System.out.print("*");
+                 System.out.print("**");
             }
                  System.out.println();
         }
