b64c59e4df6585ba4009b96b52fa55616ff12abb
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-24 06:43:57 -1100

    Day-3

---
+++ Day3(contol flow).java
@@ -0,0 +1,12 @@
+ class Day3 {
+    public static void main(String[] args){
+        int x = 20;
+        int y = 18;
+        if(y>x){
+            System.out.println("y is greater than x");
+        }else{
+            System.out.println("x is greater than y");
+        }
+    }
+    
+}
