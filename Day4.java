0c9b84160061b08178cb0bcf4db2075b420f78fb
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-26 06:06:47 -1100

    Day-4

---
+++ Day4.java
@@ -0,0 +1,13 @@
+public class Day4 {
+    public static void main(String[] args){
+        int n = 5;
+        if(n>=0){
+            System.out.println("pos");
+        }else{
+            System.out.println("neg");
+        }
+        for(int i=1;i<=10;i++){
+            System.out.println(n*i);
+        }
+    }
+}
