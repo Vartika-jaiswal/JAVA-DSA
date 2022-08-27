3c22fa9b4a20b3b73951e39f94c7cdd369deea67
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-27 07:09:43 -1100

    Day-4

--- Day_4.java
+++ Day_4.java
@@ -1,13 +1,16 @@
-public class Day_4 {
+class Day_4{
     public static void main(String[] args){
-        int n = 5;
+        int n = 19;
         if(n>=0){
             System.out.println("pos");
         }else{
             System.out.println("neg");
         }
-        for(int i=1;i<=10;i++){
-            System.out.println(n*i);
+        if(n%2!=0){
+            System.out.println("THE NO. IS PRIME");
+        }else{
+            System.out.println("NOT PRIME");
         }
+        
     }
 }
