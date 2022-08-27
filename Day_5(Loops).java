5c3c3736557964ffc5b9d52f7173202b5b286da5
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-08-27 09:33:31 -1100

    Day-5

--- Day_5(Loops).java
+++ Day_5(Loops).java
@@ -1,15 +1,10 @@
-class Day_5 {
+class Day{
     public static void main(String[] args){
-        int a = 4;
-        int b = 6;
-        int hcf = 1;
-        for(int i=1;i <= a|| i <= b;++i){
-            if(a%i==0 && b%i==0){
-                hcf = i;
-            }
-        }
-        System.out.println("hcf is:" +hcf);
+    int n=4;
+      if(n%2==0){
+         System.out.println("even");
+    }else{
+        System.out.println("odd");
     }
+   }
 }
-     
-    
