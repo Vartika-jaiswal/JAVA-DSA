0338aa5cf123cdf270a284c4bcdba86d3151438b
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-09-04 07:40:47 +1400

    Day-7

---
+++ largest_and_smallest_numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class largest_and_smallest_numbers {
+    public static void main(String[] args) {
+        System.out.println("Please enter N:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            System.out.printf("please enter %d numbers %n",n);
+            int current = sc.nextInt();
+            if(current>largest){
+                largest = current;
+            }
+            if(current<smallest){
+                smallest = current;
+            }
+        }System.out.println("largest no.:" +largest);
+        System.out.println("smallest no.:" +smallest);
+
+      
+}
+}
