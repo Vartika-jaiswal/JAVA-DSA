98f7d3f981e4e3b06dd27647f5fb6bb027c10d6e
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-09-06 09:23:53 +1400

    Day-8

--- add_two_numbers(functions).java
+++ add_two_numbers(functions).java
@@ -1,8 +1,8 @@
  class add_two_numbers {
     public static void main(String[] args){
-        add(5,10);
+        add(-4,10);
     }
-    public static void add(int num1,int num2){
+    public static void add(int num1, int num2){
         System.out.println(num1+num2);
     }
 }
