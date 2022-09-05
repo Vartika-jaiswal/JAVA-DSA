cf6a68d2aea2b103387ca6cd3b787fbd1115e51d
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-09-06 09:26:07 +1400

    Day-8

---
+++ print_spartaa.java
@@ -0,0 +1,8 @@
+public class print_spartaa {
+    public static void main(String[] args){
+        sparta();
+    }
+    public static void sparta(){
+        System.out.println("this is spartaaa!");
+    }
+}
