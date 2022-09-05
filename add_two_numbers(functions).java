5d91247d3c70c702ab8ed2f6318a7dd45a493004
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-09-06 09:18:59 +1400

    Day-8

---
+++ add_two_numbers(functions).java
@@ -0,0 +1,8 @@
+ class add_two_numbers {
+    public static void main(String[] args){
+        add(5,10);
+    }
+    public static void add(int num1,int num2){
+        System.out.println(num1+num2);
+    }
+}
