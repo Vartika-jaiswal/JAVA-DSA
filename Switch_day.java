cfcd9627a4927e8b4c7a23d1725f453491b5c900
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-09-18 08:49:08 +1400

    Day-14

---
+++ Switch_day.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Switch_day {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int day = sc.nextInt();
+        sc.close();
+        switch(day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+        }
+    }
+}
