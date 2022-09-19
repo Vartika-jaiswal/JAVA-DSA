11cec675df80bde391ae96f157ced2b35f269a64
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-09-20 09:22:34 +1400

    Day-15

---
+++ average_value_array.java
@@ -0,0 +1,14 @@
+public class average_value_array {
+    public static void main(String[] args){
+        double array[] = {10,2,3,4.3,5.6,6,7,8,9};
+        int n=array.length;
+        averagevalue(array,n);
+    }public static void averagevalue(double array[],int n){
+        double sum = 0 ;
+        double result = 0;
+        for(int i=0;i<n;i++){
+            sum+=array[i];
+             result = sum/n;
+        } System.out.println("The average of the given values in array is"+result);
+    }
+}
