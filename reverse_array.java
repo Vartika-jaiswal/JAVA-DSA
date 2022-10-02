3f1f80a70506e020fc30e5c4866ffbb707cfe487
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-10-03 09:00:34 +1400

    Day-18

---
+++ reverse_array.java
@@ -0,0 +1,24 @@
+
+public class reverse_array {
+
+	static void reverse(int a[], int n)
+	{
+		int[] b = new int[n];
+		int j = n;
+		for (int i = 0; i < n; i++) {
+			b[j - 1] = a[i];
+			j = j - 1;
+		}
+
+		System.out.println("Reversed array is: \n");
+		for (int k = 0; k < n; k++) {
+			System.out.print(" " +b[k]);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		int [] arr = {10, 20, 30, 40, 50};
+		reverse(arr, arr.length);
+	}
+}
