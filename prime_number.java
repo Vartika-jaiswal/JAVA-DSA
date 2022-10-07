7a3bb7d6f2f14a9ac2211d9a09738ea18cc18c22
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-10-08 08:57:10 +1400

    Day-20

---
+++ prime_number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class prime_number {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int temp=0;
+        for(int i=2;i<n-1;i++){
+            if(n%i==0){
+                temp=temp+1;
+            }
+        }if(temp>0){
+            System.out.println("The number is not prime");
+        }else{
+            System.out.println("The number is prime");
+        }sc.close();
+    }
+}
