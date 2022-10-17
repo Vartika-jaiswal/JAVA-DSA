ca2c41393dec0a9acc732f99104d8f8a981cb70e
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-10-18 07:28:54 +1400

    Day-27

---
+++ star_pattern_another_logic(7).java
@@ -0,0 +1,24 @@
+/* star pattern 7
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+star pattern 7*/
+import java.util.Scanner;
+class star_pattern {
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+   int n=sc.nextInt();
+   for(int i=0;i<=n;i++){
+      for(int j=0;j<=n;j++){
+        if(j<i){
+            System.out.print(" ");
+        }else{
+            System.out.print(" *");
+        }
+      }System.out.println();
+   }sc.close();
+    
+  }
+}
