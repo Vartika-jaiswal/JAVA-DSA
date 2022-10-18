9a26eff08d284e38d9a771583f25f9ed50ed6f57
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-10-19 08:09:28 +1400

    Day-28

---
+++ star_pattern(8).java
@@ -0,0 +1,24 @@
+/* star pattern 8
+ *
+   *
+     *
+       *
+         *
+star pattern 8*/
+import java.util.Scanner;
+class star_pattern {
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+   int n=sc.nextInt();
+   for(int i=0;i<=n;i++){
+      for(int j=0;j<=i;j++){
+        if(i>=2&&j<=i-1){
+            System.out.print(" ");
+        }else{
+            System.out.print("*");
+        }
+      }System.out.println();
+   }sc.close();
+    
+  }
+}
