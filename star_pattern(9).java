e9bd27ac309bbf092d81d80c23e3577a6867c759
Vartika-jaiswal <Jaiswalvartika664@gmail.com>
2022-10-21 08:08:52 +1400

    Day-30

---
+++ star_pattern(9).java
@@ -0,0 +1,21 @@
+/* star pattern 9
+ * * * * 
+ * * * * 
+ * * * * 
+ * * * *  
+star pattern 9*/
+import java.util.Scanner;
+class star_pattern9 {
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number: ");
+   int n=sc.nextInt();
+   for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        System.out.print(" *");
+    }System.out.println();
+}sc.close();
+
+    
+  }
+}
