ecd498f8fc96a29173030519d404d308510930d1
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2022-12-15 14:09:49 +0530

    Day-39

--- Functions/Stringfunction.java
+++ Functions/Stringfunction.java
@@ -1,9 +1,15 @@
 package Functions;
 
+import java.util.Scanner;
+
 public class Stringfunction {
     public static void main(String[] args) {
-        String personalised=greet("Vartika jaiswal!");
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter the name:");
+        String name=sc.next();
+        String personalised=greet(name);
         System.out.println(personalised);
+        sc.close();
     }
     static String greet(String name){
         String message="Hello "+name;
