4782496ac6294f41fb11f22a9ac95be7fdbbe215
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2022-12-15 13:37:51 +0530

    Day-39

---
+++ Functions/string_example.java
@@ -0,0 +1,12 @@
+package Functions;
+
+public class string_example {
+    public static void main(String[] args) {
+        String message=greet();
+        System.out.println(message);
+    }
+    static String greet(){
+         String Greetings="how are you";
+         return Greetings;
+    }
+}
