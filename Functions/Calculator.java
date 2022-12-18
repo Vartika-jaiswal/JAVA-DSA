255e5be604cdf7749259c8738d826b778bed702a
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2022-12-19 03:28:27 +0530

    Day-42

---
+++ Functions/Calculator.java
@@ -0,0 +1,45 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int ans=0;
+        while(true){
+            System.out.println("Enter the operator:");
+            char op=sc.next().trim().charAt(0);
+           
+            if(op=='+' || op=='-' || op=='*' || op=='/' || op=='%'){
+                System.out.println("Enter the first number:");
+                int num1=sc.nextInt();
+                System.out.println("Enter the second number:");
+                int num2=sc.nextInt();
+                if(op=='+'){
+                    ans=num1+num2;
+                }
+                if(op=='-'){
+                    ans=num1-num2;
+                }
+                if(op=='*'){
+                    ans=num1*num2;
+                }
+                if(op=='/'){
+                    if(num2!=0){
+                        ans=num1/num2;
+                    }
+                }
+                if(op=='%'){
+                    ans=num1%num2;
+                }
+                 else if(op=='x' || op=='X'){
+                   break;
+                }
+                else{
+                    System.out.println("Invalid input!!");
+                }System.out.println(ans);
+            }
+        }
+        sc.close();
+    }
+}
