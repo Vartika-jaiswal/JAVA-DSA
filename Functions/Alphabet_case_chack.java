1fc87bc9330a324b19933ce35b187da6e8443e34
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2022-12-19 01:43:14 +0530

    Day-42

---
+++ Functions/Alphabet_case_chack.java
@@ -0,0 +1,17 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Alphabet_case_chack {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the alphabet");
+        char ch=in.next().trim().charAt(0);
+        if(ch>='a' && ch<='z'){
+            System.out.println("Lowercase");
+        }else{
+            System.out.println("Uppercase");
+        }
+        in.close();
+    }
+}
