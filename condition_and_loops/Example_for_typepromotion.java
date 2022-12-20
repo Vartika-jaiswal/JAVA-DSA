d393bde4b709feaa9cc2160aa688994c4b06803b
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2022-12-21 01:29:48 +0530

    Day-43

---
+++ condition_and_loops/Example_for_typepromotion.java
@@ -0,0 +1,17 @@
+package condition_and_loops;
+
+public class Example_for_typepromotion {
+    public static void main(String[] args) {
+        byte b=42;
+        char c='a';
+        short s=1024;
+        int i=50000;
+        float f =5.67f;
+        double d=0.1234;
+        double result=(f * b) + (i / c) + (d * s);
+        System.out.println((f * b) + " " + (i / c)+ " " +(d * s));
+        System.out.println(result);
+        // char c = 'A';
+        System.out.println(c);
+    }
+}
---
+++ condition_and_loops/typecasting.java
@@ -0,0 +1,16 @@
+package condition_and_loops;
+
+public class typecasting {
+    public static void main(String[] args) {
+        // typecasting
+        int num= (int)(67.46f);
+        System.out.println(num);
+        // automatic type promotion in expressions
+        int a=257;
+        byte b=(byte)(a);
+        System.out.println(b); //257%256=1
+        
+
+
+    }
+}
