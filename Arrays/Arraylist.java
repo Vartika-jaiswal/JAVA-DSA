2b8f1ebc8ce1e374cf7858186bfcc116322a003c
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2022-12-23 22:38:52 +0530

    day-45

---
+++ Arrays/Arraylist.java
@@ -0,0 +1,18 @@
+package Arrays;
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+        list.add(67);
+        list.add(56);
+        list.add(5);
+        list.add(6);
+        list.add(78);
+        System.out.println(list);
+        System.out.println(list.contains(67));
+        list.set(1,54);
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+    }
+}
