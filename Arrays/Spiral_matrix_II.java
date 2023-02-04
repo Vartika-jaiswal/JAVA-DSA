66e6c6ddaeaf912a9d739c8be4126af9a95426cf
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-02-04 22:51:35 +0530

    Day-58

---
+++ Arrays/Spiral_matrix_II.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Spiral_matrix_II {
+    public static void main(String[] args) {
+        int n=3;
+        int[][] matrix=new int[n][n];
+        int num=1;
+        int l=0,r=n-1,t=0,b=n-1,d=0;
+        while(l<=r && t<=b){
+            if(d==0){
+                for(int i=l;i<=r;i++){
+                     matrix[t][i]=num;
+                     num++;
+                }
+                d=1;t++;
+            }else if(d==1){
+                for(int i=t;i<=b;i++){
+                     matrix[i][r]=num;
+                     num++;
+                }
+                d=2;r--;
+            }
+            else if(d==2){
+                  for(int i=r;i>=l;i--){
+                     matrix[b][i]=num;
+                     num++;
+                }
+                d=3;b--;
+            }
+            else if(d==3){
+               for(int i=b;i>=t;i--){
+                     matrix[i][l]=num;
+                     num++;
+                }
+                d=0;l++;
+            }
+        }
+        System.out.println(Arrays.toString(matrix));
+    }
+}
