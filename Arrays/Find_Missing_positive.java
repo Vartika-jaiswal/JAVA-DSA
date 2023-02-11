de9f9ec6374cd7942a388a8c975e0769d48f59ed
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-02-12 02:26:43 +0530

    Day-64

---
+++ Arrays/Find_Missing_positive.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Find_Missing_positive {
+    public static void main(String[] args) {
+        int[] nums={3,4,-1,1};
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int element=nums[i];
+        
+        if(element>=1 && element<=n){
+            int chair=element-1;
+            if(nums[chair]!=element){
+              int temp=nums[chair];
+              nums[chair]=nums[i];
+              nums[i]=temp;
+              i--;
+            }
+        }
+        }
+        for(int i=0;i<n;i++){
+            if(i+1!=nums[i]){
+                System.out.println(i+1);
+                break;
+            }
+        }
+    }
+}
