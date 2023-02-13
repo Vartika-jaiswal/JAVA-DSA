f99ae68310cf01b19933310eb4aef8455e4cf9c3
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-02-14 01:57:28 +0530

    Day-65

---
+++ Arrays/Binary_search/Ceiling_of_a_number.java
@@ -0,0 +1,26 @@
+package Arrays.Binary_search;
+
+public class Ceiling_of_a_number {
+    public static void main(String[] args) {
+        int [] arr={2,3,5,9,14,16,18};
+        System.out.println(Ceiling(arr,15));
+   }
+   static int Ceiling(int arr[],int target){
+       int start=0;
+       int  end=arr.length-1;
+           
+       while(start<=end){
+           int mid=start+(end-start)/2;
+           if(arr[mid]<target){
+               start=mid+1;
+           }else if(arr[mid]>target){
+               end=mid-1;
+           }else{
+               return mid;
+           }
+
+       }
+       return start;
+           
+       }
+}
