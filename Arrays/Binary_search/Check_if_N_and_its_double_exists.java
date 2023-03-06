2516fc7e695a539ff6fc91ecdca48f0164ea646b
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-03-07 02:29:24 +0530

    Day-83

---
+++ Arrays/Binary_search/Check_if_N_and_its_double_exists.java
@@ -0,0 +1,31 @@
+package Arrays.Binary_search;
+
+import java.util.Arrays;
+
+public class Check_if_N_and_its_double_exists {
+    public static void main(String[] args) {
+        int [] arr={10,2,5,3};
+        System.out.println(checkIfExist(arr));
+    }
+    public static boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            int target=arr[i]*2;
+            int start=0;
+            int end=arr.length-1;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(arr[mid]==target && mid!=i){
+                    return true;
+                }
+                else if(arr[mid]<target){
+                      start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
