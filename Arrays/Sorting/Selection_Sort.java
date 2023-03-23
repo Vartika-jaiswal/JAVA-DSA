481c14097797109c4e4dcd19b6fcc94ae3783ac7
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-03-24 03:05:19 +0530

    Day-88

---
+++ Arrays/Sorting/Selection_Sort.java
@@ -0,0 +1,32 @@
+package Arrays.Sorting;
+
+import java.util.Arrays;
+
+public class Selection_Sort {
+    public static void main(String[] args) {
+        int[] arr={3,1,5,4,2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-i-1;
+            int maxIndex=getmaxindex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+    private static int getmaxindex(int[] arr,int start,int end){
+        int max=start;
+        for(int i=start;i<=end;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+}
