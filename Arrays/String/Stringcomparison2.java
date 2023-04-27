588ceadfd9e4e4f2781cf6df29a6c9d550d44f42
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-04-28 02:35:42 +0530

    Day-92

---
+++ Arrays/String/Stringcomparison2.java
@@ -0,0 +1,9 @@
+package Arrays.String;
+
+public class Stringcomparison2 {
+    public static void main(String[] args) {
+        String a= new String("vartika");
+        String b= new String("vartika");
+        System.out.println(a==b);
+    }
+}
