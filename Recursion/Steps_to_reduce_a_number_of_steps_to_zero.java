5f238849535314a9add7b027ad807d74cf53d1ce
Vartika-jaiswal <jaiswalvartika664@gmail.com>
2023-07-15 03:30:27 +0530

    Day-97

--- Recursion/Steps_to_reduce_a_number_of_steps_to_zero.java
+++ Recursion/Steps_to_reduce_a_number_of_steps_to_zero.java
@@ -3,7 +3,7 @@ package Recursion;
 public class Steps_to_reduce_a_number_of_steps_to_zero {
     public static void main(String[] args) {
         int num=41;
-        System.out.println(numberOfSteps( num););
+        System.out.println(numberOfSteps( num));
     }
     public int numberOfSteps(int num) {
         return helper(num,0);
